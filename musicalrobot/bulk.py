7ca02cbc68e90e7b7cb3d4477d2d9a647cc1c795
Sage Scheiwiller <sschei@uw.edu>
2020-02-13 21:34:35 -0800

    edits to temperature profiles

--- musicalrobot/bulk.py
+++ musicalrobot/bulk.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import musicalrobot
+import os
 
 from musicalrobot import edge_detection as ed
 from musicalrobot import pixel_analysis as pa
